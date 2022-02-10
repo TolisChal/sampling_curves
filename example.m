4907262f9099447849c84889f5f7178ae02f1fdc
TolisChal <tolis.chal@gmail.com>
2022-02-10 19:33:27 +0200

    edit example

--- example.m
+++ example.m
@@ -1,10 +1,8 @@
 %generate a curve in 3-dimensions of degree 3
 coeffs = randn(3,4);
 
-a = -1;
-b = 1;
+epsilon = 0.1;
+N = 100;
 
-N = 1000;
-
-[samples, t_vals] = SampleParamCurve(coeffs, a, b, N);
+[samples, t_vals] = SampleParamCurve(coeffs, N, epsilon);
 fig = plot_points_on_3d_curve(coeffs,t_vals);
